a78b05b54f5a2694e6e90041d08a0acb0db5cd1d
Алексей Богун <t71alexb@yandex.ru>
2020-01-22 15:36:28 +0200

    Add correction function

--- sudoku-generator/Program.cs
+++ sudoku-generator/Program.cs
@@ -22,9 +22,29 @@ namespace sudoku_generator
                     Console.Write(matrix[i,j]);
                 Console.WriteLine();
             }
+            Console.WriteLine();
 
-            Console.WriteLine("Hello World!");
             Console.ReadKey(); 
+        }
+
+        private bool isCorrect(int[,] matrix, int line, int column, int num)
+        {
+            bool result = true;
+            int size = matrix.Length;
+            int cellSize = (int)Math.Sqrt(size);
+            int cell = (line / cellSize) * cellSize + (column / cellSize);
+
+            for (int i = 0; i < size; ++i)
+                if (matrix[i, column] == num) return false;
+
+            for (int j = 0; j < size; ++j)
+                if (matrix[line, j] == num) return false;
+
+            for (int i = 0; i < cellSize; ++i)
+                for (int j = 0; j < cellSize; ++j)
+                    ;
+
+                        return result;
         }
     }
 }
