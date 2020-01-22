ac04e0fbb6a0908017db3fcc2e787df863fb1a1f
Алексей Богун <t71alexb@yandex.ru>
2020-01-22 17:10:48 +0200

    Added generator

--- sudoku-generator/Program.cs
+++ sudoku-generator/Program.cs
@@ -24,15 +24,29 @@ namespace sudoku_generator
             }
             Console.WriteLine();
 
+            Random rand = new Random();
+
+            //RandGeneratingSudoku(matrix, rand);
+            Console.WriteLine();
+
+
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                    Console.Write(matrix[i, j]);
+                Console.WriteLine();
+            }
+
             Console.ReadKey(); 
         }
 
         private bool isCorrect(int[,] matrix, int line, int column, int num)
         {
-            bool result = true;
             int size = matrix.Length;
             int cellSize = (int)Math.Sqrt(size);
             int cell = (line / cellSize) * cellSize + (column / cellSize);
+            int cellLine = (line / cellSize);
+            int cellColumn = (column / cellSize);
 
             for (int i = 0; i < size; ++i)
                 if (matrix[i, column] == num) return false;
@@ -42,9 +56,18 @@ namespace sudoku_generator
 
             for (int i = 0; i < cellSize; ++i)
                 for (int j = 0; j < cellSize; ++j)
-                    ;
+                    if (matrix[i + cellLine * cellSize, j + cellColumn * cellSize] == num) return false;
 
-                        return result;
+            return true;
+        }
+
+        private void RandGeneratingSudoku(int[,] matrix, Random rand)
+        {
+            int size = matrix.Length;
+            int line = rand.Next() / size;
+            int column = rand.Next() / size;
+            int num = rand.Next() / size;
+            if (isCorrect(matrix, line, column, num)) matrix[line, column] = num;
         }
     }
 }
